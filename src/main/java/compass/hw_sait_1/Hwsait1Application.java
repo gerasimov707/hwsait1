fef500634537789e00a24fe5ea9cc5ed179e1657
alexandr <gerasimov707@yandex.ru>
2022-05-30 19:38:56 +0300

    commit 1

---
+++ src/main/java/compass/hw_sait_1/Hwsait1Application.java
@@ -0,0 +1,13 @@
+package compass.hw_sait_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hwsait1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hwsait1Application.class, args);
+    }
+
+}
---
+++ src/test/java/compass/hw_sait_1/Hwsait1ApplicationTests.java
@@ -0,0 +1,13 @@
+package compass.hw_sait_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hwsait1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
